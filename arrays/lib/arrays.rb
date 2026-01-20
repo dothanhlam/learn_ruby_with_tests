4dfa627e753b27ae16e138c93ddc85033d7facb5
aslan <dothanhlam@gmail.com>
2026-01-21 06:58:25 +0700

    attempt new array method

--- arrays/lib/arrays.rb
+++ arrays/lib/arrays.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 def arrays
   true
 end
@@ -9,3 +11,23 @@ end
 def subtract_two_arrays(array1, array2)
   0
 end
+
+def unique_value(array)
+    Set.new(array).to_a
+end 
+
+def value_iterator(array)
+    new_array = []
+    array.each do |value|
+        new_array << value + 1
+    end
+    return new_array
+end 
+
+def value_iterator_with_index(array)
+    new_array = []
+    array.each_with_index do |value, index|
+        new_array << value + index
+    end
+    return new_array
+end 
--- arrays/spec/arrays_spec.rb
+++ arrays/spec/arrays_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe "Arrays" do
   it "substract two arrays" do
     expect(subtract_two_arrays([1, 2, 3], [1, 2, 3])).to eq(0)
   end
+
+
+  it "return the unique value" do
+    expect(unique_value([1, 2, 3, 3, 4, 4, 5])).to eq([1, 2, 3, 4, 5])
+  end
+
+  it "value iterator" do
+    expect(value_iterator([1, 2, 3])).to eq([2, 3, 4])
+  end
+
+  it "value iterator with index" do
+    expect(value_iterator_with_index([1, 2, 3])).to eq([1, 3, 5])
+  end
 end
