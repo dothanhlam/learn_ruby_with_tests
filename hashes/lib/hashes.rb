9544008c8dd6a2fe8b76e64395606965dcc0fb06
aslan <dothanhlam@gmail.com>
2026-01-31 11:05:20 +0700

    numbers to words using hash

--- hashes/lib/hashes.rb
+++ hashes/lib/hashes.rb
@@ -1,3 +1,51 @@
-def hashes
-  true
+NUMBERS_TO_WORDS = {
+  0 => 'zero',
+  1 => 'one',
+  2 => 'two',
+  3 => 'three',
+  4 => 'four',
+  5 => 'five',
+  6 => 'six',
+  7 => 'seven',
+  8 => 'eight',
+  9 => 'nine',
+  10 => 'ten',
+  11 => 'eleven',
+  12 => 'twelve',
+  13 => 'thirteen',
+  14 => 'fourteen',
+  15 => 'fifteen',
+  16 => 'sixteen',
+  17 => 'seventeen',
+  18 => 'eighteen',
+  19 => 'nineteen',
+  20 => 'twenty',
+  30 => 'thirty',
+  40 => 'forty',
+  50 => 'fifty',
+  60 => 'sixty',
+  70 => 'seventy',
+  80 => 'eighty',
+  90 => 'ninety'
+}
+
+def number_to_sentence(number)
+  return NUMBERS_TO_WORDS[number] if NUMBERS_TO_WORDS.key?(number)
+
+  if number < 100
+    tens = (number / 10) * 10
+    remainder = number % 10
+    return "#{NUMBERS_TO_WORDS[tens]} #{NUMBERS_TO_WORDS[remainder]}"
+  end
+  
+  if number < 1000
+    hundreds = number / 100
+    remainder = number % 100
+    
+    result = "#{NUMBERS_TO_WORDS[hundreds]} hundred"
+    if remainder > 0
+      result += " and #{number_to_sentence(remainder)}"
+    end
+    return result
+  end
 end
--- hashes/spec/hashes_spec.rb
+++ hashes/spec/hashes_spec.rb
@@ -1,7 +1,11 @@
 require_relative '../lib/hashes'
 
 RSpec.describe 'Hashes' do
-  it 'does something' do
-    expect(hashes).to eq(true)
+  it 'converts numbers to sentences' do
+    expect(number_to_sentence(1)).to eq('one')
+    expect(number_to_sentence(13)).to eq('thirteen')
+    expect(number_to_sentence(20)).to eq('twenty')
+    expect(number_to_sentence(21)).to eq('twenty one')
+    expect(number_to_sentence(123)).to eq('one hundred and twenty three')
   end
 end
